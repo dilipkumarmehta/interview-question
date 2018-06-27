869ebd517b8698c0acdb4333c9629ddd33cbf1cb
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2018-06-27 16:42:57 +0530

    initial commit

---
+++ src/main/java/com/interview/java8feture/Java8FetureApplication.java
@@ -0,0 +1,12 @@
+package com.interview.java8feture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8FetureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8FetureApplication.class, args);
+	}
+}
