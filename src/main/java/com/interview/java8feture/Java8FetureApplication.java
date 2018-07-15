e1587b4cefb403a68a9d65d28abd909a7d6a0a8c
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2018-07-15 17:40:57 +0530

    adde

--- src/main/java/com/interview/java8feture/Java8FetureApplication.java
+++ src/main/java/com/interview/java8feture/Java8FetureApplication.java
@@ -1,4 +1,4 @@
-package com.interview.java8feture;
+/*package com.interview.java8feture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,3 +10,4 @@ public class Java8FetureApplication {
 		SpringApplication.run(Java8FetureApplication.class, args);
 	}
 }
+*/
--- src/test/java/com/interview/java8feture/Java8FetureApplicationTests.java
+++ src/test/java/com/interview/java8feture/Java8FetureApplicationTests.java
@@ -1,4 +1,4 @@
-package com.interview.java8feture;
+/*package com.interview.java8feture;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class Java8FetureApplicationTests {
 	}
 
 }
+*/
